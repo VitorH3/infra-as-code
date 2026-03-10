43cc00c92a2804ee679f85f303c0450ac60cf983
VitorH3 <vitoraandradeh3@gmail.com>
2026-03-10 20:49:01 -0300

    new: remove s3 bucket

--- index.ts
+++ index.ts
@@ -7,14 +7,6 @@ const firstBucket = new aws.s3.Bucket("primeiro-bucket", {
     },
 });
 
-
-const secondBucket = new aws.s3.Bucket("segundo-bucket", {
-    bucket: 'second-bucket-pos-graduacao',
-    tags: {
-        IAC: 'true',
-    },
-});
-
 const ecr = new aws.ecr.Repository("primeiro-ecr", {
     name: "primeiro-ecr",
     imageTagMutability: "IMMUTABLE",
@@ -26,10 +18,6 @@ const ecr = new aws.ecr.Repository("primeiro-ecr", {
 export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
-
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
