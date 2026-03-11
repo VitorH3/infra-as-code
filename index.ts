dd55cf7b123c6d47e8b2c67a44680682b71d8694
VitorH3 <vitoraandradeh3@gmail.com>
2026-03-10 21:26:00 -0300

    new: change backend state

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import * as aws from "@pulumi/aws";
 
-const firstBucket = new aws.s3.Bucket("primeiro-bucket", {
+const firstBucket = new aws.s3.Bucket("first-bucket", {
     bucket: 'first-bucket-pos-graduacao',
     tags: {
         IAC: 'true',
     },
 });
 
-const ecr = new aws.ecr.Repository("primeiro-ecr", {
-    name: "primeiro-ecr",
+const ecr = new aws.ecr.Repository("first-ecr", {
+    name: "first-ecr",
     imageTagMutability: "IMMUTABLE",
     tags: {
         IAC: 'true',
